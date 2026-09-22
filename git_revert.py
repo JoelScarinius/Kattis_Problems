7cf0d37e6436a7493a0c2a02890dc7a1640d8be7
Joel Scarinius Stävmo <scajoe@Mac.lan>
2026-09-22 11:39:29 +0200

    ADD: solution to git revert, was possible to solve in many ways but choose my own custom solution instead of using regular expression or Counter class.

---
+++ git_revert.py
@@ -0,0 +1,25 @@
+"""
+Many ways to do this, I can use counter and count occurencies of revert, I can create my own simple counter or using regular expressions for finding patterns and count occurencies.
+"""
+
+import sys
+# from collections import Counter
+# import re
+
+text = sys.stdin.readline().strip().split("\"")
+
+# counter = Counter(text)
+
+counter = 0
+for i in text:
+    if i == "Revert ":
+        counter += 1
+    else:
+        break
+
+print("revert" if counter % 2 != 0 else "unrevert")
+
+# import re
+# import sys
+
+# print("revert" if len(re.findall(r'\bRevert\b', sys.stdin.readline())) % 2 != 0 else "unrevert")
